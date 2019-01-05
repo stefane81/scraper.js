9cda93dd31e58b399b548a4381419dafdac7e8ce
Stefan Einarsson <stefane81@gmail.com>
2019-01-05 16:57:54 +0100

    Scraping presidents and set to object

--- potusScraper.js
+++ potusScraper.js
@@ -1,20 +1,26 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
+const potusParse = require('./potusParse');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
-    .then(function(html){
+    .then(function (html) {
         // success
         const wikiUrls = [];
-        for(let i = 0; i < 45; i++){
-            wikiUrls.push($('big > a',html)[i].attribs.href)
+        for (let i = 0; i < 45; i++) {
+            wikiUrls.push($('big > a', html)[i].attribs.href)
         }
-        // console.log($('big > a', html).length);
-        // console.log($('big > a', html));
-        console.log(wikiUrls);
-    })
-    .catch(function(err){
-        // handle error
-    });
 
-    
+        return Promise.all(
+            wikiUrls.map(function (url) {
+                return potusParse('https://en.wikipedia.org' + url);
+            })
+        );
+    })
+    .then(function (presidents) {
+        console.log(presidents);
+    })
+    .catch(function (err) {
+        // handle error
+        console.log(err);
+    });
